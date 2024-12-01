71b52c8222d6fe8018bb2048a23ace12969826b9
Danila Kabanets <danila@kabanets.pro>
2024-12-01 03:18:14 +0300

    All-done

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+//Функция создания карточки
+function cardCreate(cardTitle, cardImg) {
 
-// @todo: Функция удаления карточки
+    const cardElement = cardTemplate.cloneNode(true).querySelector('.places__item');
 
-// @todo: Вывести карточки на страницу
+    cardElement.querySelector('.card__image').src = cardImg;
+    cardElement.querySelector('.card__image').alt = cardTitle;
+    cardElement.querySelector('.card__title').textContent = cardTitle;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', function () {
+        const listItem = deleteButton.closest('.places__item');
+        listItem.remove();
+    });
+    
+    placesList.append(cardElement);
+};
+
+// Вывести карточки на страницу
+initialCards.forEach(function (elem) {
+    cardCreate(elem.name, elem.link);
+});
